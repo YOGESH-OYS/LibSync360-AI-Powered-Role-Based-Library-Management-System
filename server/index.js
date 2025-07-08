d71b811a30030605a16206c40de21971731530a7
Yogesh <172988161+YOGESH-OYS@users.noreply.github.com>
2025-07-09 00:11:11 +0530

    PROXY SETUP

--- server/index.js
+++ server/index.js
@@ -25,6 +25,9 @@ const { initializeCronJobs } = require("./services/cronService");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//PROXY trust
+app.set("trust proxy", true);
+
 // Security middleware
 app.use(helmet());
 app.use(compression());
